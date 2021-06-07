f361f0587aace26224dc7992121cb0be89a3e2a8
diana-k-leon <50638528+diana-k-leon@users.noreply.github.com>
2021-06-07 19:16:46 -0300

    Add files via upload

---
+++ quedarse/sketch.js
@@ -0,0 +1,25 @@
+
+var index =0;
+let b = ["quedarse", "quedarse sentado", "quedarse dormido", "quedarse a dormir", "quedarse plantado"];
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  /*
+  for (let i = 0; i < b.length; i++) {
+    text(b[i], 20, i * 20 + 20);
+  }*/
+  textSize(30);
+  textAlign(CENTER);
+  text(b[index], width / 2, height / 2);
+}
+function mousePressed() {
+	index++;
+	if (index >= b.length) {
+		index = 0;
+	}
+
+}
