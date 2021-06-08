d497ea782452a9d5f018a4b92e027c8158748ec3
diana-k-leon <50638528+diana-k-leon@users.noreply.github.com>
2021-06-07 21:26:33 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+
+var index =0;
+let b = ["quedarse", "quedarse sentado", "quedarse dormido", "quedarse a dormir", "quedarse plantado"];
+
 function setup() {
-  // put setup code here
+  createCanvas(400, 400);
 }
 
 function draw() {
-  // put drawing code here
+  background(220);
+  /*
+  for (let i = 0; i < b.length; i++) {
+    text(b[i], 20, i * 20 + 20);
+  }*/
+  textSize(30);
+  textAlign(CENTER);
+  text(b[index], width / 2, height / 2);
+}
+function mousePressed() {
+	index++;
+	if (index >= b.length) {
+		index = 0;
+	}
+
 }
