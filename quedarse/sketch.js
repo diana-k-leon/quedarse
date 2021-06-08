10793615bcdaf3d754df657fb1c88425d3067343
diana-k-leon <50638528+diana-k-leon@users.noreply.github.com>
2021-06-08 18:57:10 -0300

    Update sketch.js

--- quedarse/sketch.js
+++ quedarse/sketch.js
@@ -1,5 +1,8 @@
 var index =0;
 let tam = 10;
+let posx = 400;
+let posy = 200;
+
 let b = ["Yo no sé de pájaros,no conozco la historia del fuego. Pero creo que mi soledad debería tener alas. AP","es demasiado tarde y no hay nada peor que un demasiado tarde CB","¿Qué más quiere, qué más quiere? Átelo pronto a su muñeca, déjelo latir en libertad, imítelo anhelante."];
 
 function setup() {
@@ -8,21 +11,21 @@ function setup() {
 
 function draw() {
   background(0);
-  /*
-  for (let i = 0; i < b.length; i++) {
-    text(b[i], 20, i * 20 + 20);
-  }*/
+ 
   fill(255);
   
   textSize(tam);
   textAlign(CENTER);
-  text(b[index], width / 2, height / 2 ,170, 180);
+  text(b[index], posx, posy ,170, 180);
 }
 function mousePressed() {
 	index++;
 	if (index >= b.length) {
 		index = 0;
-        tam = random(20);
+        
 	}
+    tam = random(10,100);
+    posx = random(10,width/2);
+    posy = random(10,height/2);
 
 }
