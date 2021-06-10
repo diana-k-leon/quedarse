8ac2ff1e2e29926ae968bb091e842f316de93fa7
diana-k-leon <50638528+diana-k-leon@users.noreply.github.com>
2021-06-09 22:01:17 -0300

    Update sketch.js

--- quedarse/sketch.js
+++ quedarse/sketch.js
@@ -1,36 +1,65 @@
-var index =0;
-let tam = 10;
-let posx = 400;
-let posy = 200;
-
 let b = ["Yo no sé de pájaros,no conozco la historia del fuego. Pero creo que mi soledad debería tener alas. AP","es demasiado tarde y no hay nada peor que un demasiado tarde CB","¿Qué más quiere, qué más quiere? Átelo pronto a su muñeca, déjelo latir en libertad, imítelo anhelante."];
 
-var fontBold;//name of the font
-
-function setup() {
-  createCanvas(400, 400);
-}
-
-function draw() {
-  background(0);
- 
-  fill(255);
-  textFont(fontBold);
- 
-  textSize(tam);
-  text(b[index], posx, posy ,170, 180);
-}
-function mousePressed() {
-	index++;
-	if (index >= b.length) {
-		index = 0;
-        
-	}
-    tam = random(10,100);
-    posx = random(10,width/2);
-    posy = random(10,height/2);
-
-}
 function preload() {
   fontBold = loadFont('maquina.ttf');
 }
+
+//textFont(fontBold);
+
+var index = 0;
+let posx = 0;
+let posy = 0;
+let cantTextos = 2;
+let b = ["DIana1","diana2","diana3","diana4","diana5","diana6","DIana 1","diana2","diana3","diana4","diana5","diana6"];
+
+var fontBold;//name of the font
+
+let textos = [];
+
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+  background(0);
+  
+  for (let i=0; i < b.length; i++) {
+     textos[i] = new BubbleText(b[i]);
+  }
+  
+}
+
+function draw() {
+  fill(255);
+  background(0);
+ 
+  for (let i=0; i < cantTextos; i++) {
+     textos[i].move();
+     textos[i].show();
+  }
+  
+}
+function mousePressed() {
+  if (cantTextos < b.length) 
+    cantTextos++;
+}
+
+class BubbleText {
+  
+  constructor(t) {
+    this.x = 0;
+    this.y = random (50,400);
+    this.text = t;
+    this.tami=random(10,40);
+    this.miLimite = random (width);
+  }
+  move() {
+    if(this.x <  this.miLimite )
+       this.x = this.x + 1;
+  }
+  show() {
+    noStroke();
+    fill(255);
+    textSize(this.tami);
+    textFont(fontBold);
+    text(this.text,this.x,this.y);
+  }
+}
+ 
